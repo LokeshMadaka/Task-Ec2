c0c9f9d4548a2a45e6617ea4184c524a6b3f4444
LokeshMadaka <lokeshmadaka1236@gmail.com>
2026-01-02 21:36:02 +0530

    commit-5

--- main.tf
+++ main.tf
@@ -57,6 +57,7 @@ resource "aws_key_pair" "myownec2key" {
 }
 
 data "aws_ami" "myownec2ami"{
+  most_recent = true
   filter {
     name="name"
     values=["ubuntu/images/hvm-ssd-gp3/ubuntu-noble-24.04-amd64-server-*"]
