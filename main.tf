4e47896eed02b41fba6ad2fa52c434c58b91bd99
LokeshMadaka <lokeshmadaka1236@gmail.com>
2026-01-02 21:30:30 +0530

    commit-4

--- main.tf
+++ main.tf
@@ -58,7 +58,7 @@ resource "aws_key_pair" "myownec2key" {
 
 data "aws_ami" "myownec2ami"{
   filter {
-    name="myownec2ami"
+    name="name"
     values=["ubuntu/images/hvm-ssd-gp3/ubuntu-noble-24.04-amd64-server-*"]
   }
   owners = ["099720109477"]
